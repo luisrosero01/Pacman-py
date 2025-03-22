10bb5c0d97a97adde56237cb1125d63e9787f302
luisrosero01 <luis.rosero01@unicatolica.edu.co>
2025-03-22 14:28:17 -0500

    Create Pacman.py

---
+++ Pacman.py
@@ -0,0 +1,37 @@
+import pygame
+import random
+import sys
+import tkinter as tk
+from tkinter import messagebox
+
+# Inicializar pygame
+pygame.init()
+
+# Definir constantes
+MAPA_ANCHO = 20
+MAPA_ALTO = 9
+ANCHO, ALTO = 1200, 800
+TAMANO_CELDA = min(ANCHO // MAPA_ANCHO, ALTO // MAPA_ALTO)
+
+pantalla = pygame.display.set_mode((MAPA_ANCHO * TAMANO_CELDA, MAPA_ALTO * TAMANO_CELDA))
+pygame.display.set_caption("Pacman")
+
+# Definir colores
+NEGRO = (0, 0, 0)
+AZUL = (0, 0, 255)
+AMARILLO = (255, 255, 0)
+BLANCO = (255, 255, 255)
+ROJO = (255, 0, 0)
+
+# Mapa del juego (0 = vacío, 1 = pared, 2 = punto)
+mapa = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 2, 2, 2, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
